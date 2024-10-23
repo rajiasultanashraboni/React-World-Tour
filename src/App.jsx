07647fa072df266a97e788e6a6460700e5111368
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-10-23 19:40:33 +0600

    React World Tour

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Countries from "./Component/Countries/Countries";
+
+function App() {
+  return (
+    <>
+      <h2>React World Tour</h2>
+      <Countries></Countries>
+    </>
+  );
+}
+
+export default App;
