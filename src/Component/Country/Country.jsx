b845850149d98229b47c6f048d8cbe453e0cb9f1
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-10-23 22:08:12 +0600

    toggle button is practices here

--- src/Component/Country/Country.jsx
+++ src/Component/Country/Country.jsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
 import "./Country.css";
 const Country = ({ country }) => {
   console.log(country);
   const { name, flags } = country;
+  const [visited, setVisited] = useState(false);
+  const handleVisited = () => {
+    setVisited(!visited);
+  };
   return (
     <div className="country">
       <h3>Name: {name?.common}</h3>
       <img src={flags.png} alt="" />
+      <button onClick={handleVisited}>{visited ? "visited" : "Going"}</button>
+      {visited ? "I have visited" : "I want to visiit"}
     </div>
   );
 };
